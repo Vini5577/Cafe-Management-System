3a992912a0b1f57225b22860072a8034d06a002e
Vini5577 <ugopessoa@gmail.com>
2024-04-17 14:35:46 -0300

    add signup api route

---
+++ backend/com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.inn.cafe.serviceImpl;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.inn.cafe.POJO.User;
+import com.inn.cafe.constents.CafeConstants;
+import com.inn.cafe.dao.UserDao;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        hiddenPassWordLog(requestMap);
+        try {
+            if (validateSignUpMap(requestMap)) {
+                User user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                    return CafeUtils.getResponseEntity("Successfuly Registered.", HttpStatus.OK);
+                } else {
+                    return CafeUtils.getResponseEntity("Email already exists.", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap) {
+        if(requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private User getUserFromMap(Map<String, String> requestMap) {
+        User user = new User();
+        String hashPassword = BCrypt.withDefaults().hashToString(12, requestMap.get("password").toCharArray());
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(hashPassword);
+        user.setStatus("false");
+        user.setRole("role");
+        return user;
+    }
+
+    public void hiddenPassWordLog(Map<String, String> requestMap) {
+        Map<String, String> mapForInsert = new HashMap<>(requestMap);
+        if(mapForInsert.containsKey("password")) {
+            mapForInsert.put("password", "*******");
+        }
+        log.info("Inside signup {}", mapForInsert);
+    }
+}
